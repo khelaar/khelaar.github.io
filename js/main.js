04a07c616bcddb8e16b166c288efd668b7143de2
khelaar <r.kordiukov@gmail.com>
2014-08-20 15:16:45 +0300

    -

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
     // Init Skrollr
     var s = skrollr.init({
         render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
         }
     });
 } )( jQuery );
@@ -34,84 +32,22 @@ $(".how-it-works").click(function(){
 
 });
 
-//disable submit button
-// $("#btn-submit").click(function(){
-//   $(this).form.submit();
-//   // $(this).attr("value", "Thanks, we'll be in touch.");
-//   // $(this).css("background-color", "green");
-//   // $(this).attr("disabled", true);
-// });
-
-
-// $("#plane").click(function(){
-
-// var curve = new CurveAnimator(
-//   [100,94], [-41,455],
-//   [193,21], [447,94]
-// );
-
-// var o = document.getElementById('#plane');
-// o.style.position = 'absolute';
-
-// curve.animate(2, function(point,angle){
-//   o.style.left = point.x+"px";
-//   o.style.top  = point.y+"px";
-//   o.style.transform =
-//     o.style.webkitTransform =
-//     o.style.MozTransform =
-//     "rotate("+angle+"deg)";
-// });
-
-// });
-
+//disable submit button and play some animation
 $("#message-form").submit(function() {   
   
     $("#btn-submit").attr("value", "Thanks, we'll be in touch.");
     $("#btn-submit").css("background-color", "green");
     $('#message-form *').attr("disabled",true);
     $('.plane').addClass("plane-fly");
-    //$('#plane').addClass("plane-rotate");
-//    animate();
-
 
 
 //custom submit function shold be added
     return false;
 });
 
-
 //preload images
-
 $.fn.preload = function() {
     this.each(function(){
         $('<img/>')[0].src = this;
     });
 };
-
-
-// function animate()
-// {
-//   var path = {
-//             start: {
-//               x: 0,
-//               y: 0,
-//               angle: 315.012,
-//               length: 0.707
-//             },
-//             end: {
-//               x: 540,
-//               y: 540,
-//               angle: 315.012,
-//               length: 0.707
-//             }
-//           };
-
-//   $('.animatingDiv').animate(
-//       {
-//         path : new $.path.bezier(path)
-//       },
-//       2000,
-//       animate
-//     );
-//     alert("s"):
-// }
