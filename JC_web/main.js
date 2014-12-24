50dc31ea7b75b711f3beeb1c265d0bd694006c84
Oris <r.kordiukov@gmail.com>
2014-12-24 22:02:33 +0200

    JC 02

--- JC_web/main.js
+++ JC_web/main.js
@@ -1,10 +1,10 @@
 // ////////////////
 //
-// var $container = $('#tiles-container');
-// var prev_element = null;
-// $('.item').hover( function() {
-//   $(this).toggleClass('w2');
-//   $(prev_element).toggleClass('w2');
-//   $container.masonry();
-//   prev_element = $(this);
-// });
+var $container = $('#tiles-container');
+var prev_element = null;
+$('.item').hover( function() {
+  $(this).toggleClass('selected');
+  $(prev_element).toggleClass('selected');
+  $container.masonry();
+  prev_element = $(this);
+});
