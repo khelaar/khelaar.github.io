e3ec0fbbca6d46ddd5cd87882d185aac1f9d7c2a
Oris <r.kordiukov@gmail.com>
2014-12-24 22:15:13 +0200

    click

--- JC_web/main.js
+++ JC_web/main.js
@@ -2,9 +2,12 @@
 //
 var $container = $('#tiles-container');
 var prev_element = null;
-$('.item').click( function() {
+$('.item').hover( function() {
   $(this).toggleClass('selected');
   $(prev_element).toggleClass('selected');
   $container.masonry();
   prev_element = $(this);
+});
+$('.item').click( function() {
+  $(this).text('Clicked');
 });
