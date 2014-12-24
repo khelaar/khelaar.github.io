7a6b81a10571c6c4b56e196a35a3e51ad2cf35a2
Oris <r.kordiukov@gmail.com>
2014-12-24 22:05:16 +0200

    hover

--- JC_web/main.js
+++ JC_web/main.js
@@ -2,7 +2,7 @@
 //
 var $container = $('#tiles-container');
 var prev_element = null;
-$('.item').hover( function() {
+$('.item').click( function() {
   $(this).toggleClass('selected');
   $(prev_element).toggleClass('selected');
   $container.masonry();
